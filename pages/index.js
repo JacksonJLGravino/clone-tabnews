f723f4e196fefae3bf8e6a06aa77643f86467ee2
Jackson Gravino <jacksonjlgravino@gmail.com>
2024-12-04 10:18:11 +0000

    dia 4

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home(){
+    return (
+    <>
+    <h1>Mire na lua. Mesmo que você erre, estará entre as estrelas.</h1>
+    <i>Norman Vincent Peale</i>
+    </>
+)
+}
+
+export default Home;
