8b0478ec5c1e44ed6d53649a02b9d34ed2ef1ec4
Jackson Gravino <jacksonjlgravino@gmail.com>
2025-01-19 18:58:53 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-function Home(){
-    return <h1>Mire na lua. Mesmo se você errar, você ficará entre as estrelas⭐</h1>
-
+function Home() {
+  return (
+    <h1>Mire na lua. Mesmo se você errar, você ficará entre as estrelas⭐</h1>
+  );
 }
 
 export default Home;
